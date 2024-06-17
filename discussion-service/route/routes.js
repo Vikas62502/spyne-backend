070f23f5aa25ce2ae98222e72c54bd4d047d06ce
vikas62502 <vikashyadav62502@gmail.com>
2024-06-17 12:54:07 +0530

    routes updated of discussion service
    routes updated of discussion service

--- discussion-service/route/routes.js
+++ discussion-service/route/routes.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import auth from '../middleware/auth.js';
 import createDiscussion from '../controller/createDiscussion.js';
+import deleteDiscussion from '../controller/deleteDiscussion.js';
+import getDiscussionByTags from '../controller/getDiscussionByTags.js';
+import updateDiscussion from '../controller/updateDiscussion.js';
 
 // initilized the router
 const router = express.Router();
 router.use(express.json()); // Parse JSON bodies
 
 router.post('/createDiscussion', auth, createDiscussion)
+router.delete('/deleteDiscussion/:discussionId', auth, deleteDiscussion)
+router.get('/getAllDiscussionsByTags', auth, getDiscussionByTags)
+router.get('/getAllDiscussionBasedOnText', auth, getDiscussionByTags)
+router.put('/updateDiscussion/:discussionId', auth, updateDiscussion)
 
 
 export default router;
--- user-service/controller/updateUser.js
+++ user-service/controller/updateUser.js
@@ -1,13 +1,14 @@
 import User from "../model/UserSchema.js";
 
 const updateUser = async (req, res) => {
+    console.log("Hello")
     try {
         const { userId } = req.params;
+        console.log(req.params, "req.params")
         // validate user ID
         if (!userId) return res.status(400).json({ message: 'User ID is required' });
 
         const { name, mobileNumber, email } = req.body;
-        if(mobileNumber.length !== 10 ) return res.status(400).json({ message: 'Mobile number should be 10 digits' });
         const user = await User.findByIdAndUpdate(userId, {
             name,
             mobileNumber,
@@ -16,6 +17,7 @@ const updateUser = async (req, res) => {
         res.json({ message: 'User modified', user });
     }
     catch (error) {
+        console.log(error);
         res.status(500).json({ error: error.message });
     }
 }
