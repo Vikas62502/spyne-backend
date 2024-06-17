9e73bac8e2a9ca459765395faa5da2da2b33791a
vikas62502 <vikashyadav62502@gmail.com>
2024-06-17 14:15:20 +0530

    comment apis testing done

--- comment-service/controller/createComment.js
+++ comment-service/controller/createComment.js
@@ -1,10 +1,13 @@
 import Comment from "../model/commentSchema.js";
+import Discussion from "../model/discussionSchema.js";
 
 const createComment = async (req, res) => {
     try {
         const { text, discussionId } = req.body;
         const comment = new Comment({ text, discussion: discussionId, user: req.user });
         await comment.save();
+
+        await Discussion.findByIdAndUpdate(discussionId, { $push: { comments: comment._id } });
         res.status(201).json({ message: 'Comment created successfully!' });
     } catch (error) {
         res.status(400).json({ error: error.message });
--- comment-service/controller/deleteComment.js
+++ comment-service/controller/deleteComment.js
@@ -1,9 +1,16 @@
 import Comment from "../model/commentSchema.js";
+import Discussion from "../model/discussionSchema.js";
 
 const deleteComment = async (req, res) => {
     try {
         const { id } = req.params;
         await Comment.findByIdAndDelete(id);
+
+        await Discussion.findByIdAndUpdate(req.body.discussionId, {
+            $pull: {
+                comments: id,
+            }
+        });
         res.status(200).json({ message: 'Comment deleted successfully!' });
     } catch (error) {
         res.status(400).json({ error: error.message });
