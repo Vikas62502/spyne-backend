d83042fd7d6353c4bc96a3f4b96d76e0cf0a13f6
vikas62502 <vikashyadav62502@gmail.com>
2024-06-17 22:21:39 +0530

    bug fixed reply to comment, delete Like

--- comment-service/controller/replyToComment.js
+++ comment-service/controller/replyToComment.js
@@ -1,22 +1,21 @@
+import Comment from "../model/commentSchema.js";
+
 const replyToComment = async (req, res) => {
     try {
         const { commentId, text } = req.body;
         const userId = req.user;
-
         const newReply = new Comment({
             user: userId,
-            discussion: null,
             text,
         });
 
         const savedReply = await newReply.save();
 
-        // adding to parent reply
         const parentComment = await Comment.findById(commentId);
         parentComment.replies.push(savedReply._id);
         await parentComment.save();
 
-        res.status(201).json(savedReply);
+        res.status(201).json(parentComment);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
--- like-service/controller/deleteLike.js
+++ like-service/controller/deleteLike.js
@@ -3,19 +3,14 @@ import Like from "../model/likeSchema.js";
 const deleteLike = async (req, res) => {
     try {
         const { likeId } = req.params;
-        const userId = req.user._id;
+        const userId = req.user;
 
-        const like = await Like.findById(likeId);
-        if (!like) {
-            return res.status(404).json({ message: 'Like not found' });
+        const removeLike = await Like.findByIdAndDelete(likeId);
+        if (!removeLike) {
+            return res.status(400).json({ message: 'Like not found' });
         }
 
-        if (like.user.toString() !== userId.toString()) {
-            return res.status(403).json({ message: 'Unauthorized to delete this like' });
-        }
-
-        await like.remove();
-        res.status(200).json({ message: 'Like removed' });
+        res.status(200).json({ message: 'Like removed', removeLike });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
